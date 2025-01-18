8852c476b5e0492f7581f4ffa3e6273df90cb2fe
Martyna <167070778+MartynaSzymanskaGitHub@users.noreply.github.com>
2025-01-18 10:34:43 +0100

    dodanie do kamery głównej

--- src/videoprocessor/cameraprocessor/MainCameraProcessing.py
+++ src/videoprocessor/cameraprocessor/MainCameraProcessing.py
@@ -2,19 +2,24 @@ from database.connector.DatabaseConnector import DatabaseConnector
 from videoprocessor.cameraprocessor import AllCamerasResources
 from videoprocessor.frameprocessor.LineDetector import LineDetector
 from videoprocessor.videomanager.CameraType import CameraType
-
-
+from videoprocessor.frameprocessor.CarDetector import CarDetector
+import cv2
 class MainCameraProcessing:
     def __init__(self, database_connector: DatabaseConnector, all_cameras_resources: AllCamerasResources):
         self.database_connector = database_connector
         self.frame = None
         self.all_cameras_resources = all_cameras_resources
         self.gate_manager = all_cameras_resources.gate_manager
+        self.car_detector = CarDetector()
 
     def run(self):
         """
             Przetwanie klatki z kamery głównej
         """
+
+        # przetwarzanie obrazu (wykrywanie i rysowanie keypoints)
+        self.car_detector.process_frame(self.frame)
+
 
         # todo - przetwarzanie klatki z kamery głównej
         LineDetector.detect_lines(self.frame, line_color=(0, 0, 255))
