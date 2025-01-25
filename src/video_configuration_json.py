e5072e885faf1c1088a22f3ed3394d9f200fda14
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2025-01-25 21:46:46 +0100

    json update

--- src/video_configuration_json.py
+++ src/video_configuration_json.py
@@ -26,12 +26,12 @@ video_configuration_json = {
             'parking_spaces': [
                 # w lewo -  w góra -
                 # Srodek
-                (170, 110, 70, 150),  # lewy górny róg
-                (245, 115, 70, 150),  # górny środek
-                (320, 120, 70, 150),  # prawy górny róg
-                (170, 265, 70, 150),  # lewy dolny róg
-                (245, 270, 70, 150),  # dolny środek
-                (320, 275, 70, 150),  # prawy prawy róg
+                (180, 130, 70, 150),  # lewy górny róg
+                (255, 130, 70, 150),  # górny środek
+                (330, 130, 70, 150),  # prawy górny róg
+                (180, 285, 70, 150),  # lewy dolny róg
+                (255, 280, 70, 150),  # dolny środek
+                (330, 275, 70, 150),  # prawy prawy róg
                 # Prawo od góry
                 (520, 10, 150, 80),
                 (520, 95, 150, 80),
@@ -47,12 +47,12 @@ video_configuration_json = {
             'parking_spaces': [
                 # w lewo -  w góra -
                 # Srodek
-                (170, 110, 70, 150),  # lewy górny róg
-                (245, 115, 70, 150),  # górny środek
-                (320, 120, 70, 150),  # prawy górny róg
-                (170, 265, 70, 150),  # lewy dolny róg
-                (245, 270, 70, 150),  # dolny środek
-                (320, 275, 70, 150),  # prawy prawy róg
+                (180, 130, 70, 150),  # lewy górny róg
+                (255, 130, 70, 150),  # górny środek
+                (330, 130, 70, 150),  # prawy górny róg
+                (180, 285, 70, 150),  # lewy dolny róg
+                (255, 280, 70, 150),  # dolny środek
+                (330, 275, 70, 150),  # prawy prawy róg
                 # Prawo od góry
                 (520, 10, 150, 80),
                 (520, 95, 150, 80),
@@ -63,8 +63,5 @@ video_configuration_json = {
             ],
             'entry_gate_height': 0,
         }
-    },
-    'kamera': {
-
     }
 }
