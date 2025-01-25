c1fae809098e5331eb20ca5d05de40a16ad8df49
Wafelek <111795716+wojciechwelfle@users.noreply.github.com>
2025-01-25 21:55:38 +0100

    json update

--- src/video_configuration_json.py
+++ src/video_configuration_json.py
@@ -1,15 +1,37 @@
 video_configuration_json = {
-    'wjazd, parkowanie  i wyjazd poprawne': {
+    'identyfikacja ludzika': {
         'parameters': {
             'parking_spaces': [
                 # w lewo -  w góra -
                 # Srodek
-                (170, 110, 70, 150),  # lewy górny róg
-                (245, 115, 70, 150),  # górny środek
-                (320, 120, 70, 150),  # prawy górny róg
-                (170, 265, 70, 150),  # lewy dolny róg
-                (245, 270, 70, 150),  # dolny środek
-                (320, 275, 70, 150),  # prawy prawy róg
+                (180, 130, 70, 150),  # lewy górny róg
+                (255, 130, 70, 150),  # górny środek
+                (330, 130, 70, 150),  # prawy górny róg
+                (180, 285, 70, 150),  # lewy dolny róg
+                (255, 280, 70, 150),  # dolny środek
+                (330, 275, 70, 150),  # prawy prawy róg
+                # Prawo od góry
+                (520, 10, 150, 80),
+                (520, 95, 150, 80),
+                (520, 190, 150, 80),
+                (520, 285, 150, 80),
+                (520, 385, 150, 90),
+                (520, 470, 150, 70),
+            ],
+            'entry_gate_height': 0,
+        }
+    },
+    'jasne niebieskie wyjazd': {
+        'parameters': {
+            'parking_spaces': [
+                # w lewo -  w góra -
+                # Srodek
+                (180, 130, 70, 150),  # lewy górny róg
+                (255, 130, 70, 150),  # górny środek
+                (330, 130, 70, 150),  # prawy górny róg
+                (180, 285, 70, 150),  # lewy dolny róg
+                (255, 280, 70, 150),  # dolny środek
+                (330, 275, 70, 150),  # prawy prawy róg
                 # Prawo od góry
                 (520, 10, 150, 80),
                 (520, 95, 150, 80),
@@ -42,17 +64,18 @@ video_configuration_json = {
             ],
             'entry_gate_height': 0,
         }
-    }, 'identyfikacja ludzika': {
+    },
+    'wjazd, parkowanie  i wyjazd poprawne': {
         'parameters': {
             'parking_spaces': [
                 # w lewo -  w góra -
                 # Srodek
-                (180, 130, 70, 150),  # lewy górny róg
-                (255, 130, 70, 150),  # górny środek
-                (330, 130, 70, 150),  # prawy górny róg
-                (180, 285, 70, 150),  # lewy dolny róg
-                (255, 280, 70, 150),  # dolny środek
-                (330, 275, 70, 150),  # prawy prawy róg
+                (170, 110, 70, 150),  # lewy górny róg
+                (245, 115, 70, 150),  # górny środek
+                (320, 120, 70, 150),  # prawy górny róg
+                (170, 265, 70, 150),  # lewy dolny róg
+                (245, 270, 70, 150),  # dolny środek
+                (320, 275, 70, 150),  # prawy prawy róg
                 # Prawo od góry
                 (520, 10, 150, 80),
                 (520, 95, 150, 80),
@@ -63,5 +86,7 @@ video_configuration_json = {
             ],
             'entry_gate_height': 0,
         }
-    }
+    },
+
+
 }
